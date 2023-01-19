ffbd7f381a02cdce4cce8363831049fe9a534f79
Ankush Ladani <astarladani@gmail.com>
2023-01-19 18:10:55 +0530

    Completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,68 @@
 import { useState } from "react";
-
+import "../../style.css";
 // import ToDo from "./ToDo";
 
 const Header = () => {
   const [inputText, setInputText] = useState("");
   const [todoList, setTodoList] = useState([]);
-  function addTodo(title) {
-    setTodoList(...todoList, title);
-    setInputText("");
+
+  function addTodo() {
+    if (inputText) {
+      setTodoList([...todoList, inputText]);
+      setInputText("");
+    }
   }
 
+  function deleteTodo(index) {
+    const updatedTodo = todoList.filter((todo, id) => {
+      return id != index;
+    });
+
+    setTodoList([...updatedTodo]);
+  }
+
+  const styleLi = {
+    textDecoration: "dashed",
+    backgroundColor: "gray",
+  };
+
   return (
-    <>
+    <div className="mainDiv">
       <div className="header">
         <div>
           <h1 className="heading">My To Do List</h1>
         </div>
-        <div>
+        <div className="inputDiv">
           <input
             onChange={(e) => setInputText(e.target.value)}
             className="inputTitle"
             type="text"
             placeholder="Title..."
           />
-          <button onClick={() => addTodo(inputText)} className="addBtn">
+          <button onClick={addTodo} className="addBtn">
             Add
           </button>
         </div>
       </div>
 
-      <ul id="myUL">
-        <li>Hit the gym</li>
-        <li className="checked">Pay bills</li>
-        <li>Meet George</li>
-        <li>Buy eggs</li>
-        <li>Read a book</li>
-        <li>Organize office</li>
-      </ul>
-    </>
+      <div className="innerDiv">
+        <ul className="lists">
+          {todoList.map((todo, index) => (
+            <div key={index} className="listDiv">
+              <li
+                style={{ display: "flex", margin: "0px 10px" }}
+                className="listItem"
+              >
+                {todo}
+              </li>
+              <button onClick={() => deleteTodo(index)} className="deleteBtn">
+                Delete
+              </button>
+            </div>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
 
